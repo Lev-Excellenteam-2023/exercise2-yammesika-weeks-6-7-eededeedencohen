f4450e3b3cf3ef188aa08eed86e56b4af94ce241
Eden Cohen <eededeedencohen@gmail.com>
2023-08-28 20:10:50 +0300

    fix 6.2 timer

--- week06/6.2.timer.py
+++ week06/6.2.timer.py
@@ -1,15 +1,18 @@
-#------------------------
-# ריצת 2000 - 6.2
-#------------------------
-
-#===========================================================================
-# timer function:
-# gets a function and unlimited number of parameters for that function.
-# returns the time it took to execute the function with those parameters.
-#===========================================================================
 import timeit
+from typing import Any
 
-def timer(func, *args):
+
+def timer(func: callable, *args: Any) -> str:
+    """
+    @summary:
+        return the time it took to execute the function with list of parameters
+    @param func: callable
+        The function that we will execute
+    @param args: Any
+        The parameters for the function that we will execute
+    @return: str
+        The time it took to execute the function with list of parameters
+    """
     start = timeit.default_timer()
     func(*args)
     end = timeit.default_timer()
@@ -17,35 +20,7 @@ def timer(func, *args):
     return str(end - start) + " seconds"
 
 
-
-# main program
 if __name__ == "__main__":
     print(f"zip: {timer(zip, [1, 2, 3], [4, 5, 6])} \n")
 
-    # print hello timer
     print(f"print: {timer(print, 'hello world')}")
-
-
-
-
-
-
-    
-
-
-
-    
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
