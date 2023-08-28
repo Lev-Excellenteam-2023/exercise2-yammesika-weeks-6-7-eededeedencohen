c83bf36b590c9673a1b5b2ad8ee819dceecdf9e4
Eden Cohen <eededeedencohen@gmail.com>
2023-08-28 20:27:15 +0300

    fix 6.3 full_names

--- week06/6.3.full_names.py
+++ week06/6.3.full_names.py
@@ -1,38 +1,25 @@
-#------------------
-# ואלה שמות - 6.3
-#------------------
+def full_names(first_names: list[str], last_names: list[str], min_length: int = 2) -> list[str]:
+    """
+    @summary:
+        return a list of full names that are longer than the minimum length.
+    @param first_names: list[str]
+        list of First names
+    @param last_names: list[str]
+        list of Last names
+    @param min_length: int
+        The minimum length of the full name to be returned (default 2)
+    @return: list[str]
+        list of full names that are longer than the minimum length.
+    """
+    all_full_names_combinations = [first_name + " " + last_name for first_name in first_names for last_name in last_names]
+    return [full_name for full_name in all_full_names_combinations if len(full_name) >= min_length]
 
-#================================================================
-# full_names function:
-# get two lists: list of First names and list of Last names.
-# get a minimum length of full name.
-# return a list of full names that are longer than the minimum length.
-#================================================================
-def full_names(first_names, last_names,min_length=-1):
-    return [f'{first_name.title()} {last_name.title()}'
-             for first_name in first_names for last_name in last_names 
-             if len(first_name+last_name)+1 >= min_length or min_length == -1] 
+
+FIRST_NAMES = ['avi', 'moshe', 'yaakov']
+LAST_NAMES = ['cohen', 'levi', 'mizrahi']
+MINIMUM_LENGTH = 8
 
 
-# main program
 if __name__ == "__main__":
-    first_names = ['avi', 'moshe', 'yaakov']
-    last_names = ['cohen', 'levi', 'mizrahi']
-
-    min_length = 10
-    print(full_names(first_names, last_names,min_length))
-    print(f"There are {len(full_names(first_names, last_names,min_length))} full names \n")
-
-    print (full_names(['avi', 'moshe', 'yaakov'], ['cohen', 'levi', 'mizrahi']))
-    print(f"There are {len((full_names(first_names, last_names)))} full names")
-
-
-    
-
-
-
-
-
-
-
-
+    print(full_names(FIRST_NAMES, LAST_NAMES, MINIMUM_LENGTH))
+    print(f"There are {len((full_names(FIRST_NAMES, LAST_NAMES, MINIMUM_LENGTH)))} full names")
