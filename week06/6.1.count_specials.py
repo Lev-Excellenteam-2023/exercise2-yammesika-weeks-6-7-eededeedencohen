bfaac6f3bce07814da59a3192dfbd4fcdd1a655c
Eden Cohen <eededeedencohen@gmail.com>
2023-08-28 19:30:28 +0300

    fix 6.1

--- week06/6.1.count_specials.py
+++ week06/6.1.count_specials.py
@@ -1,32 +1,25 @@
-#================================
-# 6.1 - ספירת מלאי
-#================================
 from check_type_decorator import check_type
 
 
-#--------------------------------------------------------------------------
-# return how many numbers that smaller than n and are divisible by 3 or 7
-#--------------------------------------------------------------------------
 @check_type(int)
-def count_specials(n):
-    s = set()
-    for i in range(1, n):
-        if i % 3 == 0 or i % 7 == 0:
-            s.add(i)
-    return len(s)
+def count_specials(n: int) -> int:
+    """
+     @summary:
+        return how many numbers that smaller than n and are divisible by 3 or 7
+    @param n: int
+        The number up to which we will check numbers that are divisible by 3 or 7
+    @return: int
+        The number of numbers that smaller than n and are divisible by 3 or 7
+    """
+    divisible_by_3_or_7 = set(divisible_number for divisible_number in range(1, n)
+                              if divisible_number % 3 == 0 or divisible_number % 7 == 0)
+    return len(divisible_by_3_or_7)
 
 
-# main function
 def main():
     n = 100
     print("There are", count_specials(n), "numbers that smaller than", n, "and are divisible by 3 or 7")
 
 
-# main program
 if __name__ == "__main__":
     main()
-    
-
-
-
-    
