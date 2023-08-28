903305cafefdf5eed0903a8fe104e1a4526d1c16
Eden Cohen <eededeedencohen@gmail.com>
2023-08-28 19:39:42 +0300

    fix 6.1 find_special_state

--- week06/6.1.find_special_state.py
+++ week06/6.1.find_special_state.py
@@ -1,16 +1,6 @@
-#=======================
-# שטוחלנדיה - 6.1
-#=======================
-
-
-# set of the letters in the 1st row of the keyboard
-topRow = set("qwertyuiop")
-
-# set of the letters in the 2nd row of the keyboard
-midRow = set("asdfghjkl")
-
-# set of the letters in the 3rd row of the keyboard
-botRow = set("zxcvbnm")
+TOP_ROW_KEYBOARD = set("qwertyuiop")
+MIDDLE_ROW_KEYBOARD = set("asdfghjkl")
+BOTTOM_ROW_KEYBOARD = set("zxcvbnm")
 
 # insert all the words in the resources/states.txt file to a set
 states = set()
@@ -18,19 +8,18 @@ with open("resources/states.txt", "r") as f:
     for line in f:
         states.add(line.strip().lower())
 
-#-------------------------------------------------------------
-# find_special_state function:
-# returns the state that has all the letters in the same row
-#-------------------------------------------------------------
-def find_special_state():
+
+def find_special_state() -> str:
+    """
+    @summary:
+        return the first state that its name is typed using only one row of the keyboard
+    @return: str
+        The first state that its name is typed using only one row of the keyboard
+    """
     for state in states:
-        if topRow.issuperset(state) or midRow.issuperset(state) or botRow.issuperset(state):
+        if TOP_ROW_KEYBOARD.issuperset(state) or MIDDLE_ROW_KEYBOARD.issuperset(state) or BOTTOM_ROW_KEYBOARD.issuperset(state):
             return state
 
-# main program
+
 if __name__ == "__main__":
     print(find_special_state())
-
-
-
-
